59e35f7aac203bf11bba1964a98d034f0c9c2d8d
rifkisrg <rfksrg@gmail.com>
2019-11-12 11:01:50 +0700

    add 2 new methods get

--- application/models/Buku.php
+++ application/models/Buku.php
@@ -6,8 +6,24 @@ class Buku extends CI_Model{
         $this->load->database();
     }
 
-    public function getDataBuku($idBuku){
+    public function getDataBukuById($idBuku){
         $query = $this->db->select('*')->from('buku')->where('id_buku', $idBuku);
+
+        $result = $query->get()->result();
+
+        return $result;
+    }
+
+    public function getDataBukuByName($namaBuku){
+        $query = $this->db->select('*')->from('buku')->where('nama', $namaBuku);
+
+        $result = $query->get()->result();
+
+        return $result;
+    }
+
+    public function getDataBukuByCategory($categoryBuku){
+        $query = $this->db->select('*')->from('buku')->where('category', $categoryBuku);
 
         $result = $query->get()->result();
 
